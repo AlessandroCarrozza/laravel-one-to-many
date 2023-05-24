5d9dd714d975db82f56a9392eabe47744e0ee5db
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-05-24 18:10:43 +0200

    migrazione per la fk

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
 
 class TypeSeeder extends Seeder
 {
